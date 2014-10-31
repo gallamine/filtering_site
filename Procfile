ac2ec45461ab6f6bfca97e3ca07ca5dd79fc5242
William Cox <gallamine@gmail.com>
2014-10-30 20:42:22 -0400

    figuring out gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi.wsgi --log-file -
+web: gunicorn wsgi.py --log-file -
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ site.addsitedir(os.path.dirname(__file__))
 
 logging.basicConfig(stream = sys.stderr)
 
-import app as application
+import filtering_site as application
