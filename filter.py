efa84e1638dbe147f306ac524f06a4a8ab2db834
William Cox <gallamine@gmail.com>
2014-11-08 12:34:11 -0500

    Added plots to show tabs (filter/<fid>/plots/taps) and filters can now run data (filter/<fid>/run?data=1,2,3,4,5). The taps can be returned as a json dit also via filter/<fid>/taps

--- filter.py
+++ filter.py
@@ -2,18 +2,96 @@ import numpy as np
 import io
 from flask import send_file
 from scipy import signal
+import json
 
 class Filter():
 
-    b = []
+    fir_coeff = []
+    config = {}
+    data = []
+    fid = 0
 
-    def make_FIR_LPF(self,num_taps=40,cutoff=0.5,window=('kaiser',8)):
+    supported_types = ['lpf','hpf']
 
-        self.b = signal.firwin(int(num_taps), float(cutoff), window=window)
-        return True
+    def __init__(self,fid):
+        self.fid = fid
 
-    def return_spectrum(self):
-        w, h = signal.freqz(self.b)
+    def makeFilter(self,filt_type,config):
+        if filt_type in self.supported_types:
+            self.config = config
+            if filt_type == "lpf":
+                return self._makeFIR_LPF(config['num_taps'],config['cutoff'],config['window'])
+
+            elif filt_type == "hpf":
+                return False
+        else:
+            return {'error':'Unsupported filter {0}'.format(filt_type)}
+
+    def _makeFIR_LPF(self,num_taps=40,cutoff=0.5,window=('kaiser',8)):
+        try:
+            self.fir_coeff = signal.firwin(int(num_taps), float(cutoff), window=window)
+            self.data = np.zeros(int(num_taps))
+            return True
+        except Exception as e:
+            print e
+            return False
+
+    def runFilter(self, new_data):
+        '''
+
+        :param new_data: New data to be filtered
+        :return: the filtered data of the same length as the input data
+        '''
+        new_data = [float(i) for i in new_data.split(',') if i is not '']
+        try:
+            if len(new_data) > len(self.fir_coeff):
+
+                self.data = new_data[-len(fir_coeff):] #Only save the last bit of data to give filter history
+                new_data = np.concatenate(self.data,new_data)  # Filter history
+                filtered_signal = signal.lfilter(self.fir_coeff,1.0, new_data)
+                return filtered_signal[len(fir_coeff):]
+            else:
+                self.data = np.roll(self.data,-len(new_data))  #Shift old data out
+                self.data[-len(new_data):] = new_data     #Shift in new data
+                filtered_signal = signal.lfilter(self.fir_coeff, 1.0, self.data)
+                return filtered_signal[-len(new_data):]
+        except Exception as e:
+            print e
+            return {'error':'??'}
+
+    def returnTaps(self):
+        '''
+
+        :return: taps in json format
+        '''
+        return self.fir_coeff.tolist()
+
+
+    def returnTapPlot(self):
+        '''
+
+        :return: A matplotlib image of the filter taps
+        '''
+
+        import matplotlib.pyplot as plt
+
+        fig = plt.figure()
+        plt.title('{0} Filter taps'.format(self.fid))
+        plt.stem(self.fir_coeff)
+        plt.axis('tight')
+        buf = io.BytesIO()
+        plt.savefig(buf,format= 'png')
+        buf.seek(0)
+        return send_file(buf,\
+                     attachment_filename='taps.png',\
+                     mimetype='image/png')
+
+    def returnSpectrum(self):
+        '''
+
+        :return: A matplotlib image of the filter spectrum
+        '''
+        w, h = signal.freqz(self.fir_coeff)
 
         import matplotlib.pyplot as plt
         fig = plt.figure()
--- filtering_site.py
+++ filtering_site.py
@@ -2,24 +2,103 @@ from flask import Flask
 from flask import request
 from flask.ext import restful
 import filter
+import uuid
+import json
+import db
 
 app = Flask(__name__)
 api = restful.Api(app)
 
-class HelloWorld(restful.Resource):
-    def get(self):
-        return {'hello': 'world'}
+all_filters = {}
 
-api.add_resource(HelloWorld, '/api')
+class FilterHandle(restful.Resource):
+    def createNewFilter(self):
+        fid = uuid.uuid4()
+        try:
+            all_filters[str(fid)] = filter.Filter(fid)
+
+            filt_type = request.args.get('type','lpf')
+            num_taps = request.args.get('num_taps',40)
+            cutoff = request.args.get('cutoff',0.5)
+            # TODO: What other windows does scipy support?
+            window=('kaiser',8)
+            config = {'num_taps':num_taps,'cutoff':cutoff,'window':window}
+            all_filters[str(fid)].makeFilter(filt_type,config)
+            # Save to disk the list of filters
+            db.writeKey('all_filters',all_filters)
+            return {'fid':str(fid)}
+        except Exception as e:
+            return {'error':e}
+
+    def get(self,fid,command):
+        '''
+
+        :type fid: unicode
+        :type command: unicode
+        :return:
+        '''
+        if fid == "new":
+            # Create a new filter object
+            return self.createNewFilter()
+
+        else:
+            if fid in all_filters.keys():
+
+                if command == "taps":
+                    return {'taps':all_filters[fid].returnTaps()}
+                elif command == "run":
+                    # Run the filter using the data provided in data
+                    data = request.args.get('data','')
+
+                    if data == '':
+                        return {'error':'Send me some data!'}
+                    else:
+                        data_resp = all_filters[fid].runFilter(data)
+                        # TODO: Save only the upated one
+                        if isinstance(data_resp, dict):
+                            return data_resp
+                        else:
+                            db.writeKey('all_filters',all_filters)
+                            return {'fid':fid,'data_in':data,'data_out':data_resp.tolist()}
+                else:
+                    return {'hello': 'world','fid':fid}
+            else:
+                return {'error':"Invalid filter ID"}
+
+
+api.add_resource(FilterHandle, '/filter/<fid>/<command>')
+
+class FilterPlotting(restful.Resource):
+    def get(self,fid,command):
+        '''
+
+        :type fid: unicode Filter ID. You get this by callling filter/new
+        :type command: unicode Plot commands (spectrum and taps
+        :return: whatever is supposed to come out
+        '''
+        if fid in all_filters.keys():
+            if command == "spectrum":
+                # Plot the filter spectrum
+                # TODO: Accept sample rate to scale the plot
+                return all_filters[fid].returnSpectrum()
+
+            elif command == "taps":
+                 # Plot the filter taps
+                return all_filters[fid].returnTapPlot()
+
+        else:
+            return {'error':"Invalid filter ID"}
+
+api.add_resource(FilterPlotting, '/filter/<fid>/plots/<command>')
 
 @app.route('/')
 def hello_world():
     #args_dict = request.args
     #return args_dict
-    return 'Hello World!'
+    return 'Go to /filter/new to get a filter handle!<br>'
 
-@app.route('/spectrum/')
-def show_spectrum():
+@app.route('/spectrum/<filt_type>')
+def show_spectrum(filt_type):
     f = filter.Filter()
     try:
         num_taps = request.args.get('num_taps','')
@@ -29,7 +108,29 @@ def show_spectrum():
         cutoff = 0.5
     window=('kaiser',8)
     f.make_FIR_LPF(num_taps,cutoff,window)
-    return f.return_spectrum()
+    return f.returnSpectrum()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Load filters from db
+    try:
+        all_filters = db.readKey('all_filters')
+        if all_filters is False:
+            all_filters = {}
+    except:
+        print "No filters saved"
+
+
+    app.run(passthrough_errors = True)
+    #app.run(debug=True)
