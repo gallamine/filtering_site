8fb626e6e1f2f7ca4a9c68e39f4868b35f1085f6
William Cox <gallamine@gmail.com>
2014-12-11 21:30:54 -0500

    Cleaned up cruft in app. Moved to MongoDB. Changed db to a class. Verified filtering works now.

--- db.py
+++ db.py
@@ -1,28 +1,70 @@
 import shelve
+from pymongo import MongoClient
+import pickle
+import os
+global db
+
+class Database():
+
+    db = None
+
+    def __init__(self, username, password):
+        self.db = self.initialize_db_conn(username, password)
+
+    def initialize_db_conn(self, username, password):
+
+        """
+        Connect to the MongoDB server and save connection
+        :return:
+        """
+
+        DB_NAME = "filter_site"
+
+        client = MongoClient('mongodb://{0}:{1}@ds063160.mongolab.com:63160/{2}'.format(username, password, DB_NAME))
+        db = client[DB_NAME]
+
+        return db
 
 
+    def saveFilter(self, Filter):
+        """
+        Save a serialized filter object into the MongoDB using the _id of fid
+        :param Filter: Filter object
+        :return: True if successfully written
+        """
+
+        collection_name = "filters"
+        try:
+            collection = self.db[collection_name]
+            collection.insert({"_id": str(Filter.fid), "object": pickle.dumps(Filter)})
+        except Exception as e:
+            return False, e
 
 
-def writeKey(key,value):
+    def loadAllFilters(self):
+        """
+        Load all of the filters in the "filters" collection in to a dict of Python Filter objects
+        :return: Dict of Filter objects fid: object paris
+        """
 
-    try:
-        d = shelve.open('filter_site_krufty.db')
-        d[key] = value
-        d.close()
-        return True
-    except:
+        filter_collection = self.db["filters"]
+        all_filters = {}
+        for filter in filter_collection.find():
+            all_filters[filter["_id"]] = pickle.loads(filter["object"])
+
+        print "Loaded {0} filters".format(len(all_filters))
+        return all_filters
+
+
+    def writeKey(self, key, value):
+        """
+        Write a key:value into the database
+        :param key: key name
+        :param value:  value of the key to write
+        :return: True or False and exception
+        """
         return False
 
-def readKey(key):
-    try:
-        d = shelve.open('filter_site_krufty.db')
-        ret_val = d[key]
-        d.close()
-        return ret_val
-    except Exception as e:
-        print e
-        return False
 
 
-def removeKey(key):
-    del d[key]
+
--- filtering_site.py
+++ filtering_site.py
@@ -1,96 +1,122 @@
+import db as dbclass
 from flask import Flask
 from flask import request
 from flask.ext import restful
+from flask import jsonify
 import filter
 import uuid
 import json
-import db
 
-app = Flask(__name__)
+
+class MyServer(Flask):
+
+    def __init__(self, *args, **kwargs):
+        super(MyServer, self).__init__(*args, **kwargs)
+
+        try:
+            APPLICATION_PATH = "/Users/william/Documents/filtering_site/"
+            f = open(APPLICATION_PATH + '.mongo', 'r')
+            username, password = f.read().split("\t")
+            # Load filters from db
+            self.db = dbclass.Database(username, password)
+
+            self.all_filters = self.db.loadAllFilters() #db.readKey('all_filters')
+            if self.all_filters is False:
+                self.all_filters = {}
+
+        except:
+            print "No filters saved"
+
+app = MyServer(__name__)
 api = restful.Api(app)
 
-all_filters = {}
 
-class FilterHandle(restful.Resource):
-    def createNewFilter(self):
-        fid = uuid.uuid4()
-        try:
-            all_filters[str(fid)] = filter.Filter(fid)
+def createNewFilter(request):
+    """
 
-            filt_type = request.args.get('type','lpf')
-            num_taps = request.args.get('num_taps',40)
-            cutoff = request.args.get('cutoff',0.5)
-            # TODO: What other windows does scipy support?
-            window=('kaiser',8)
-            config = {'num_taps':num_taps,'cutoff':cutoff,'window':window}
-            all_filters[str(fid)].makeFilter(filt_type,config)
-            # Save to disk the list of filters
-            db.writeKey('all_filters',all_filters)
-            return {'fid':str(fid)}
-        except Exception as e:
-            return {'error':e}
+    :return:
+    """
+
+    fid = uuid.uuid4()
+    try:
+        app.all_filters[str(fid)] = filter.Filter(fid)
+
+        filt_type = request.args.get('type','lpf')
+        #TODO: Put an upper limit on the tap size
+        num_taps = request.args.get('num_taps', 40)
+        cutoff = request.args.get('cutoff',0.5)
+        # TODO: What other windows does scipy support?
+        window = ('kaiser', 8)
+        config = {'num_taps': num_taps, 'cutoff': cutoff, 'window': window}
+        app.all_filters[str(fid)].makeFilter(filt_type, config)
+
+        app.db.saveFilter(app.all_filters[str(fid)])
+        return jsonify({'fid': str(fid)})
+    except Exception as e:
+        #TODO: This is a big security no-no
+        return jsonify({'error': e})
 
 
-    def get(self,command):
-        '''
+@app.route('/filter/<command>')
+def filter_commands(command):
+    """
+    Operations to do to a filter pass the FID via fid=XXXXX arguemnt
+    :param command:
+    :return:
+    """
 
-        :type fid: unicode
-        :type command: unicode
-        :return:
-        '''
+    if request.method == 'GET':
+
         if command == "new":
             # Create a new filter object
-            return self.createNewFilter()
+            return createNewFilter(request)
 
         else:
-            fid = request.args.get('fid','')
-            if fid in all_filters.keys():
+            fid = request.args.get('fid', '')
+            if fid in app.all_filters.keys():
 
                 if command == "taps":
-                    return {'taps':all_filters[fid].returnTaps()}
+                    return jsonify({'taps': app.all_filters[fid].returnTaps()})
                 elif command == "run":
                     # Run the filter using the data provided in data
-                    data = request.args.get('data','')
+                    data = request.args.get('data', '')
 
                     if data == '':
-                        return {'error':'Send me some data!'}
+                        return jsonify({'error': 'Send me some data!'})
                     else:
-                        data_resp = all_filters[fid].runFilter(data)
-                        # TODO: Save only the upated one
+                        data_resp = app.all_filters[fid].runFilter(data)
                         if isinstance(data_resp, dict):
-                            return data_resp
+                            return jsonify(data_resp)
                         else:
-                            db.writeKey('all_filters',all_filters)
-                            return {'fid':fid,'data_in':data,'data_out':data_resp.tolist()}
+                            app.db.saveFilter(app.all_filters[fid])
+                            return jsonify({'fid': fid, 'data_in': data, 'data_out': data_resp.tolist()})
                 else:
-                    return {'hello': 'world','fid':fid}
+                    return jsonify({'hello': 'world', 'fid': fid})
             else:
-                return {'error':"Invalid filter ID"}
+                return jsonify({'error': "Invalid filter ID"})
 
-api.add_resource(FilterHandle, '/filter/<command>')
 
-class FilterPlotting(restful.Resource):
-    def get(self,fid,command):
-        '''
+@app.route('/filter/<fid>/plots/<command>')
+def filter_plotting(fid, command):
+    '''
 
-        :type fid: unicode Filter ID. You get this by callling filter/new
-        :type command: unicode Plot commands (spectrum and taps
-        :return: whatever is supposed to come out
-        '''
-        if fid in all_filters.keys():
+    :type fid: unicode Filter ID. You get this by callling filter/new
+    :type command: unicode Plot commands (spectrum and taps
+    :return: whatever is supposed to come out
+    '''
+    if request.method == 'GET':
+        if fid in app.all_filters.keys():
             if command == "spectrum":
                 # Plot the filter spectrum
                 # TODO: Accept sample rate to scale the plot
-                return all_filters[fid].returnSpectrum()
+                return app.all_filters[fid].returnSpectrum()
 
             elif command == "taps":
                  # Plot the filter taps
-                return all_filters[fid].returnTapPlot()
+                return app.all_filters[fid].returnTapPlot()
 
         else:
-            return {'error':"Invalid filter ID"}
-
-api.add_resource(FilterPlotting, '/filter/<fid>/plots/<command>')
+            return jsonify({'error': "Invalid filter ID"})
 
 @app.route('/')
 def hello_world():
@@ -100,6 +126,11 @@ def hello_world():
 
 @app.route('/spectrum/<filt_type>')
 def show_spectrum(filt_type):
+    """
+    Draw a spectrum for the specififed filter type. Don't save the filter
+    :param filt_type:
+    :return:
+    """
     f = filter.Filter()
     try:
         num_taps = request.args.get('num_taps','')
@@ -107,8 +138,8 @@ def show_spectrum(filt_type):
     except:
         num_taps = 40
         cutoff = 0.5
-    window=('kaiser',8)
-    f.make_FIR_LPF(num_taps,cutoff,window)
+    window = ('kaiser',8)
+    f.make_FIR_LPF(num_taps, cutoff, window)
     return f.returnSpectrum()
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -124,14 +155,6 @@ def login():
     '''
 
 if __name__ == '__main__':
-    # Load filters from db
-    try:
-        all_filters = db.readKey('all_filters')
-        if all_filters is False:
-            all_filters = {}
-    except:
-        print "No filters saved"
-
 
     app.run(passthrough_errors = True)
     #app.run(debug=True)
